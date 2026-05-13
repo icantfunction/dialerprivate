6e51ff3d40989286c09b10f47ed28056685088cf
icantfunction <opendavidfunction@gmail.com>
2026-05-13 12:36:15 -0400

    Configure deployed dialer API endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DIALER_CONFIG = window.DIALER_CONFIG || {
-  // callApi: 'https://your-api-id.execute-api.us-east-1.amazonaws.com/prod/api/call',
-  // callStopApi: 'https://your-api-id.execute-api.us-east-1.amazonaws.com/prod/api/call/stop',
-  // ccpUrl: 'https://manualdialer20260414195157.my.connect.aws/ccp-v2/',
+  callApi: 'https://7g0v2he198.execute-api.us-east-1.amazonaws.com/api/call',
+  callStopApi: 'https://7g0v2he198.execute-api.us-east-1.amazonaws.com/api/call/stop',
+  ccpUrl: 'https://manualdialer20260414195157.my.connect.aws/ccp-v2/',
 };
